043eda063e2ac73e7d531d328b6b7ce6ade1980d
dongwudi <550293874@qq.com>
2019-04-18 14:59:40 +0800

    模板语法

--- src/main.js
+++ src/main.js
@@ -3,66 +3,23 @@ import Vue from 'vue/dist/vue.esm';
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'hello vue.js!'
+    message: 'hello vue.js!',
+    rawHtml: '<span style="color:red;">this is span</span>',
+    dynamicId: 'tx',
+    isButtonDisabled: true ,//null undefined false
+    number: 1,
+    ok: true,
+    url: 'https://www.baidu.com',
+    attributename: 'href', // 全小写
+    eventname: 'click'
   },
   methods: {
-    changeMessage() {
-      this.message = 'update hello vuejs'
+    doSomething () {
+      console.log('v-on')
     },
-    destroy(){
-      this.$destroy()
+    onSubmit () {
+      console.log('prevent')
     }
-  },
-  beforeCreate() {
-    console.group(`beforeCreate --- this.message is %c ${this.message}`,"color:red")
-    console.log('%c%s',"color:red",`组件实例刚被创建，组件属性计算之前，如data属性`)
-    console.groupEnd();
-  },
-  created () {
-    console.group(`created --- this.message is %c ${this.message}`,"color:red")
-    console.group(`created --- this.$el is %c ${this.$el}`,"color:red")
-    console.log('%c%s',"color:red",`组件实例创建完成，属性已经绑定，DOM还未生成，$el属性还不存在`)
-    console.groupEnd();
-  },
-  beforeMount() {
-    console.group(`beforeMount --- app.innerHtml is %c ${document.getElementById('app').innerHTML}`,"color:red")
-    console.group(`created --- this.$el is %c ${this.$el}`,"color:red")
-    console.log('%c%s',"color:red",`模板编译/挂载之前`)
-    console.groupEnd();
-  },
-  mounted() {
-    console.group(`mounted --- app.innerHtml is %c ${document.getElementById('app').innerHTML}`,"color:red")
-    console.log('%c%s',"color:red",`模板编译/挂载之后`)
-    console.groupEnd();
-  },
-  beforeUpdate() {
-    console.group('beforeUpdate----------')
-    console.log('%c%s',"color:red",`el: ${this.$el.innerHTML}`)
-    console.log('%c%s',"color:red",`this.message: ${this.message}`)
-    console.log('%c%s',"color:red",`message已经为更新后的数据，但是DOM还没有更新，组件更新之前`)
-    console.groupEnd();
-  },
-  updated() {
-    console.group('updated----------')
-    console.log('%c%s',"color:red",`el: ${this.$el.innerHTML}`)
-    console.log('%c%s',"color:red",`this.message: ${this.message}`)
-    console.log('%c%s',"color:red",`组件更新之后`)
-    console.groupEnd();
-  },
-  beforeDestroy() {
-    console.group(`beforeDestroy --- app._isVue is %c ${this._isVue}`,"color:red")
-    console.log('%c%s',"color:red",`组件销毁前调用`)
-    console.groupEnd();
-  },
-  destroyed() {
-    console.group(`destroyed --- app._isVue is %c ${this._isVue}`,"color:red")
-    console.log('%c%s',"color:red",`组件销毁后调用，vue控制失效`)
-    setTimeout(()=>{
-      this.message = 'destroyed'
-      console.log('%c%s',"color:red",`${this.message}`)
-      console.log('%c%s',"color:red",`dom中的message并没有变化，${this.$el.innerHTML}`)
-    },1000)
-    console.groupEnd();
   }
 });
 
