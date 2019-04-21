d805f7cc57592edaa68b2f8485e4dddb111f6264
dongwudi <550293874@qq.com>
2019-04-21 21:33:52 +0800

    表单输入绑定

--- src/main.js
+++ src/main.js
@@ -3,36 +3,21 @@ import Vue from 'vue/dist/vue.esm';
 const app = new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    n: 0
-  },
-  methods: {
-    addCounter (ev) {
-      console.log(ev.target.tagName)
-      this.counter ++;
-    },
-    addCounter1 (n) {
-      this.n = n;
-      this.counter += n;
-    },
-    doThis () {
-      console.log('dothis')
-    },
-    do1(){
-      console.log('do')
-    },
-    dothat(){
-      console.log('dothat')
-    },
-    dononce(){
-      console.log('doonce')
-    },
-    scrollev(){
-      console.log('1')
-    },
-    keyenv(){
-      console.log('keyup')
-    }
+    message: '',
+    checked: true,
+    checkedNames: [],
+    picked: '',
+    selected: 'A',
+    selectedarr:[],
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ],
+    radiostr:'',
+    toggle:false,
+    msg:'msg',
+    age: 1
   }
 });
 
