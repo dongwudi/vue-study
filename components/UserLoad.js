02e009a707f5af4ae0475addb3e2c9bdb7efe17d
dongwudi <550293874@qq.com>
2019-04-24 15:43:54 +0800

    嵌套路由

---
+++ components/UserLoad.js
@@ -0,0 +1,8 @@
+export default {
+  template: `
+  <div>
+    user load
+    <img src="https://static.web.sdo.com/gmys/20190319_invite/one_bg_1.jpg?3a120f9756b30be33e2db1e8d22f92f6">
+    <img src="https://static.web.sdo.com/gmys/20190219_web/0326pc.jpg" >
+  </div>`
+}
