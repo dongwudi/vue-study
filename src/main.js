623965d495b167332aaf435a730ab5e98bfa067d
dongwudi <550293874@qq.com>
2019-04-29 17:24:42 +0800

    vuex start

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue/dist/vue.esm';
 import router from '../router';
+import store from '../vuex';
 
 const app = new Vue({
   el: '#app',
   router,
   data: {
 
+  },
+  computed: {
+    count () {
+	    return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+    	store.commit('decrement')
+    }
   }
 });
 
